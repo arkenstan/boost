cd4de670bdc2503840e72a94ff928c7a46b1ebfc
arkenstan <anshulwagadre@gmail.com>
2021-02-21 18:06:03 +0530

    added obstacle

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
     AudioSource engineSound;
-    bool engineSoundToggle;
+
+    private const string friendlyTag = "friendly";
+    private const string fuelTag = "fuel";
+    private const string deadTag = "dead";
 
     // Start is called before the first frame update
     void Start()
@@ -22,22 +27,51 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
-    private void ProcessInput(){
+    private void OnCollisionEnter(Collision collision) {
+        print("Collided");
+        switch(collision.gameObject.tag){
+            case friendlyTag:{
+                print("OK");
+                break;
+            }
+            case fuelTag:{
+                print("FUEL");
+                break;
+            }case deadTag:{
+                print("DEAD");
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    private void ProcessRotate(){
+        rigidBody.freezeRotation = true;
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        if( Input.GetKey(KeyCode.A) ){
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }else if( Input.GetKey(KeyCode.D) ){
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+        rigidBody.freezeRotation = false;
+    }
+
+    private void ProcessThrust(){
+        float thrustSpeed = mainThrust * Time.deltaTime;
         if(Input.GetKey(KeyCode.Space)){
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustSpeed);
             if(!engineSound.isPlaying){
                 engineSound.Play();
             }
         }else{
             engineSound.Stop();
         }
-
-
-        if( Input.GetKey(KeyCode.A) ){
-            transform.Rotate(Vector3.forward);
-        }else if( Input.GetKey(KeyCode.D) ){
-            transform.Rotate(-Vector3.forward);
-        }
-
     }
+
+    private void ProcessInput(){
+        ProcessThrust();
+        ProcessRotate();
+    }
+
 }
